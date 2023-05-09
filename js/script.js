2d5e350f04ff3e880cffdc105b66aa011c24df43
MatteoMancuso-92 <Matteotrieste@outlook.com>
2023-05-09 18:59:57 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,146 +1,94 @@
-const arrayImage = [
-    {
-        image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
-];
+const app = Vue.createApp({
+	data() {
+		return {
+			colorBorder: 'red',
+			activeIndex: 0,
+			sliderDirection: 1,
+			isAutorun: true,
+			autorunTime: 1000,
+			idAutorun: null,
+			arrImages: [
+				{
+					image: 'img/01.webp',
+					title: 'Marvel\'s Spiderman Miles Morale',
+					text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+				},
+				{
+					image: 'img/02.webp',
+					title: 'Ratchet & Clank: Rift Apart',
+					text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+				},
+				{
+					image: 'img/03.webp',
+					title: 'Fortnite',
+					text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+				},
+				{
+					image: 'img/04.webp',
+					title: 'Stray',
+					text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+				},
+				{
+					image: 'img/05.webp',
+					title: "Marvel's Avengers",
+					text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+				},
+			],
+		};
+	},
+	methods: {
+		showPrevSlide() {
+			// settiamo il nuovo valore di active index
+			this.activeIndex--;
+			if (this.activeIndex < 0) {
+				this.activeIndex = this.arrImages.length - 1;
+			}
+		},
 
-setInterval(() => {      
+		showNextSlide() {
+			// settiamo il nuovo valore di active index
+			this.activeIndex++;
+			if (this.activeIndex >= this.arrImages.length) {
+				this.activeIndex = 0;
+			}
+		},
 
-    	title[activeIndex].classList.remove('active');
-		text[activeIndex].classList.remove('active');
-        listHighlighted[activeIndex].classList.remove('active');
-		listThumbs[activeIndex].classList.remove('active');
-		// settiamo il nuovo valore di active index
-		activeIndex++;
-		if (activeIndex >= listHighlighted.length) {
-			activeIndex = 0;
+		setActiveIndex(i) {
+			this.activeIndex = i;
+		},
+
+		toggleAutorun() {
+			this.isAutorun = !this.isAutorun;
+		},
+
+		runSlider() {
+			if (this.isAutorun) {
+				this.idAutorun = setInterval(
+					() => this.sliderDirection == 1 ? this.showNextSlide() : this.showPrevSlide(),
+					this.autorunTime
+				);
+			} else {
+				clearInterval(this.idAutorun);
+			}
+		},
+
+		invertSliderDirection() {
+			this.sliderDirection *= -1;
+		},
+
+		stopAutorun() {
+			clearInterval(this.idAutorun);
 		}
-		// alla nuova immagine attiva aggiungiamo la classe active
-        title[activeIndex].classList.add('active');
-		text[activeIndex].classList.add('active');
-        listHighlighted[activeIndex].classList.add('active');
-		listThumbs[activeIndex].classList.add('active');
+	},
+	watch: {
+		isAutorun() {
+			console.log('dal watcher');
+			this.runSlider();
+		},
+	},
+	mounted() {
+		this.runSlider();
+	},
+});
 
-	}, 2500);
-    
-
-
-const containerHighlighted = document.querySelector('.highlighted');
-const containerThumbs = document.querySelector('.thumbs');
-
-
-for (let i = 0; i < arrayImage.length; i++) {
-	containerHighlighted.innerHTML += `
-    
-        <img src="${arrayImage[i].image}" alt=""  class="${i == 0 ? 'active' : ''}">
-        <div class="title ${i == 0 ? 'active' : ''} ">${arrayImage[i].title} </div>
-        <div class="text ${i == 0 ? 'active' : ''}"> ${arrayImage[i].text}  </div>`;
-        
-        containerThumbs.innerHTML += `<img src="${arrayImage[i].image}" alt="" class="${i == 0 ? 'active' : ''}">`;
-}
-
-
-// selezionimo le immagini nell'html
-const title = document.querySelectorAll('.title');
-const text = document.querySelectorAll('.text');
-const listHighlighted = document.querySelectorAll('.highlighted img');
-// selezioniamo le miniature
-const listThumbs = document.querySelectorAll('.thumbs img');
-// selezioniamo i bottoni
-const btnPrev = document.querySelector('.btn-prev');
-const btnNext = document.querySelector('.btn-next');
-
-
-// definito una variabile che rappresenta lo stato attuale del carosello
-// cioe' l'indice dell'immagine attiva
-let activeIndex = 0;
-
-btnNext.addEventListener('click',
-	function() {
-		// dall'immagine attiva tolgo la classe active
-        title[activeIndex].classList.remove('active');
-		text[activeIndex].classList.remove('active');
-        listHighlighted[activeIndex].classList.remove('active');
-		listThumbs[activeIndex].classList.remove('active');
-		// settiamo il nuovo valore di active index
-		activeIndex++;
-		if (activeIndex >= listHighlighted.length) {
-			activeIndex = 0;
-		}
-		// alla nuova immagine attiva aggiungiamo la classe active
-        title[activeIndex].classList.add('active');
-		text[activeIndex].classList.add('active');
-        listHighlighted[activeIndex].classList.add('active');
-		listThumbs[activeIndex].classList.add('active');
-	}
-);
-
-btnPrev.addEventListener('click',
-	function() {
-		// dall'immagine attiva tolgo la classe active
-        title[activeIndex].classList.remove('active');
-		text[activeIndex].classList.remove('active');
-        listHighlighted[activeIndex].classList.remove('active');
-		listThumbs[activeIndex].classList.remove('active');
-       
-		// settiamo il nuovo valore di active index
-		activeIndex--;
-		if (activeIndex < 0) {
-			activeIndex = listHighlighted.length - 1;
-		}
-		// alla nuova immagine attiva aggiungiamo la classe active
-        
-        title[activeIndex].classList.add('active');
-		text[activeIndex].classList.add('active');
-		listHighlighted[activeIndex].classList.add('active');
-		listThumbs[activeIndex].classList.add('active');
-	}
-);
-
-// ciclo per aggiungere gli event listeners alle miniature
-for (let i = 0; i < listThumbs.length; i++) {
-	listThumbs[i].addEventListener('click',
-		function() {
-			console.log('cliccata la miniature in posizione ' + i)
-            
-            title[activeIndex].classList.remove('active');
-            text[activeIndex].classList.remove('active');
-			listHighlighted[activeIndex].classList.remove('active');
-			listThumbs[activeIndex].classList.remove('active');
-			
-            activeIndex = i;
-            
-            title[activeIndex].classList.add('active');
-		    text[activeIndex].classList.add('active');
-            listHighlighted[activeIndex].classList.add('active');
-			listThumbs[activeIndex].classList.add('active');
-		}
-	)
-}
-
-/*
-btnNext ---> al click fai function() {...}
-btnPrev ---> al click fai function() {...}
-thumb0 ----> al click fai function() {...}
-thumb1 ----> al click fai function() {...}
-thumb3 ----> al click fai function() {...}
-thumb4 ----> al click fai function() {...}
-*/
+app.mount('.slider');
